f1012927c74ea6c6dbd41c92d9121a5475bca9d6
saasbook <saasbook@saasbook.(none)>
2012-12-28 16:28:27 -0800

    add_user

---
+++ db/migrate/20121229002357_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+      t.string :address
+      t.integer :mobile
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
