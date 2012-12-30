9cd100cda33df65187b488d82329b1bc658fbae5
saasbook <saasbook@saasbook.(none)>
2012-12-30 06:54:59 -0800

    establish relation between car and user

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,4 @@
 class Car < ActiveRecord::Base
   attr_accessible :carmodel, :price, :user_id, :year
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :address, :email, :mobile, :name, :password
+   has_many :cars
 end
